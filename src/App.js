71f7f0d07cb5164a2ae5d9d24c2764b447ce723f
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-10-09 23:20:22 +0600

    Added some changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
-  return <h1>Hi!</h1>;
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <Button variant="primary"> Button </Button>
+    </>
+  );
 }
 
 export default App;
