93e719e4c0f4645d8452b9d2c6bf8ba7350fc3c5
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-10-10 00:33:35 +0600

    Added the main nav tegs in Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,40 +1,56 @@
 import React, { Component } from "react";
-import { Navbar } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  FormControl,
+  Container,
+  Form,
+  Button,
+} from "react-bootstrap";
 import logo from "./logo192.png";
 
 export default class Header extends Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="nd" bg="light" variant="light">
-        <Container>
-          <Navbar.brand href="/">
-            <img
-              src={}
-              heigth="30"
-              width="30"
-              className="d-inline-block align-top"
-              alt="Logo"
-            />
-          </Navbar.brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav classname="mr-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/about">About us</Nav.Link>
-              <Nav.Link href="/contacts">Contacts</Nav.Link>
-              <Nav.Link href="/blog">Blog</Nav.Link>
-            </Nav>
-            <Form inline>
-              <FormControl
-                type="text"
-                placeholder="Search"
-                className="mr-sm-2"
+      <>
+        <Navbar
+          fixed="top"
+          collapseOnSelect
+          expand="md"
+          bg="light"
+          variant="light"
+        >
+          <Container>
+            <Navbar.Brand href="/">
+              <img
+                src={logo}
+                heigth="30"
+                width="30"
+                className="d-inline-block align-top"
+                alt="Logo"
               />
-              <Button variant="outline-info">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+              React site
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav classname="mr-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/about">About us</Nav.Link>
+                <Nav.Link href="/contacts">Contacts</Nav.Link>
+                <Nav.Link href="/blog">Blog</Nav.Link>
+              </Nav>
+              <Form inline>
+                <FormControl
+                  type="text"
+                  placeholder="Search"
+                  className="mr-sm-1"
+                />
+                <Button variant="outline-info">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </>
     );
   }
 }
