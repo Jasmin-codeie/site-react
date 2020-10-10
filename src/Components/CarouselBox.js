15033461d613ca73164f5266abfba0176e4e75ef
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-10-10 22:56:29 +0600

    Made a carousel

---
+++ src/Components/CarouselBox.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import first from "../assets/first.jpg";
+import second from "../assets/second.jpg";
+
+export class CarouselBox extends Component {
+  render() {
+    return (
+      <Carousel>
+        <Carousel.Item>
+          <img height={660} className="d-block w-100" src={first} alt="First" />
+          <Carousel.Caption>
+            <h3>First image</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            height={660}
+            className="d-block w-100"
+            src={second}
+            alt="Second"
+          />
+          <Carousel.Caption>
+            <h3>Second image</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+  }
+}
+
+export default CarouselBox;
