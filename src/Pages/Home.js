2c6685a6eddec058547e0d7d31c737d449bf7184
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-10-12 11:52:48 +0600

    Added one Card in the Home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,38 @@
 import React, { Component } from "react";
 import CarouselBox from "../Components/CarouselBox";
+import { Card, Container, CardDeck, Button } from "react-bootstrap";
 
 export default class Home extends Component {
   render() {
-    return <CarouselBox />;
+    return (
+      <>
+        <CarouselBox />
+        <Container>
+          <h3 className="text-center m-4">Our team</h3>
+          <CardDeck>
+            <Card>
+              <Card.Img
+                variant="top"
+                src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+              />
+              <Card.Body>
+                <Card.Title>Developers</Card.Title>
+                <Card.Text>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </Card.Text>
+                <Button variant="dark">About Team</Button>
+              </Card.Body>
+            </Card>
+          </CardDeck>
+        </Container>
+      </>
+    );
   }
 }
