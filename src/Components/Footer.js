7ebaa4055e7cbca4aa6238fcb0fffc492bb25a8a
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-10-14 21:33:33 +0600

    Added some content to Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import classes from "./Footer.module.css";
 
 export default class Footer extends Component {
   render() {
     return (
       <div>
-        <Footer>
-          <div></div>
+        <Footer className={classes.Footer}>
+          <div>Contacts and Support</div>
         </Footer>
       </div>
     );
